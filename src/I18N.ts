68e44b843338c24252641e1e8115be4919287a44
Kai Miyamoto <kaihogehoge@gmail.com>
2024-12-06 01:46:08 +0900

    Change dictionaries to dictionary

--- src/I18N.ts
+++ src/I18N.ts
@@ -1,7 +1,7 @@
 import i18next from "i18next";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector/cjs";
-import en from "./dictionaries/en.json";
-import ja from "./dictionaries/ja.json";
+import en from "./dictionary/en.json";
+import ja from "./dictionary/ja.json";
 
 const STORAGE_KEY = "in_renderer_i18n" as const;
 
